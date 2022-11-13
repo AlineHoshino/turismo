a005c5a0df7d53e94da11bc7f21cb59205739783
ALINE <hoshinoaline@yahoo.com.br>
2022-11-13 20:03:09 -0300

    Filtros usando querystring

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -11,7 +11,31 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset = queryset.filter(nome__iexact=nome)
+
+        if descricao:
+            queryset = queryset.filter(descricao=descricao)
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
+
 
     # def list(self, request, *args, **kwargs):
     #     return Response({'teste': 123}) - exemplo de get All sendo sobrescrito
