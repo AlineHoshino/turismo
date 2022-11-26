5199fb166231231a06a9fde690a325429420f319
ALINE <hoshinoaline@yahoo.com.br>
2022-11-26 18:02:34 -0300

    corrige file

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,10 +5,12 @@ from atracoes.models import Atracao
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from endereco.api.serializers import EnderecoSerializer
+from endereco.models import Endereco
+
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
-    endereco = EnderecoSerializer(read_only=True)
+    endereco = EnderecoSerializer()
     descricao_completa = SerializerMethodField()
     class Meta:
         model = PontoTuristico
@@ -23,8 +25,14 @@ class PontoTuristicoSerializer(ModelSerializer):
     def create(self, validated_data):
         atracoes = validated_data["atracoes"]
         del validated_data["atracoes"]
+
+        endereco = validated_data["endereco"]
+        del validated_data["endereco"]
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
+
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
 
         return ponto
 
