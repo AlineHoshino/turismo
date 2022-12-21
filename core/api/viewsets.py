e8d6c2294cdfab009d4ae7299f0bc021d7500ee1
ALINE <hoshinoaline@yahoo.com.br>
2022-12-20 21:19:00 -0300

    adiciona paginação

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -10,7 +11,6 @@ from .serializers import PontoTuristicoSerializer
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-
     serializer_class = PontoTuristicoSerializer
     filter_backends = (SearchFilter,)
     # permission_classes = (IsAuthenticated,)
@@ -21,6 +21,7 @@ class PontoTuristicoViewSet(ModelViewSet):
         id = self.request.query_params.get('id', None)
         nome = self.request.query_params.get('nome', None)
         descricao = self.request.query_params.get('descricao', None)
+
         queryset = PontoTuristico.objects.all()
 
         if id:
@@ -43,10 +44,19 @@ class PontoTuristicoViewSet(ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
 
-
     # def list(self, request, *args, **kwargs):
     #     return Response({'teste': 123}) - exemplo de get All sendo sobrescrito
 
     # @action(methods=['get'], detail=True)
     # def denunciar(self, request, pk=None):
     #     return Response({'teste': 123})
+
+    # @action(methods=['post'], detail=True)
+    # def associa_atracoes(self, request, id):
+    #     atracoes = request.data['ids']
+    #     ponto = PontoTuristico.objects.get(id=id)
+    #
+    #     ponto.atracoes.set(atracoes)
+    #
+    #     ponto.save()
+    #     return HttpResponse("ok")
